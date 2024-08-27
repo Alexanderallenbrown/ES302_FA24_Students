e459967580fed1eb611097e5e39da7e63ad987a1
Alexander Brown <brownaa@lafayette.edu>
2024-08-26 20:53:00 -0400

    syllabus link fix

--- source_notebook_archiver.py
+++ source_notebook_archiver.py
@@ -26,7 +26,7 @@ opener = """
 <div style="border: solid 2px black; border-radius: 10px; text-align: center; width: 50vw;margin-left: auto; margin-right: auto">
 <br><br>
 
-<h2 style="text-align: center"><a href="hhttps://docs.google.com/document/d/12d84ZSdh6Ft9LoFXpwEcwnretBL_8YEED3gPnJtXnDU/pub">Course Syllabus FA24</a></h2>
+<h2 style="text-align: center"><a href="https://docs.google.com/document/d/12d84ZSdh6Ft9LoFXpwEcwnretBL_8YEED3gPnJtXnDU/pub">Course Syllabus FA24</a></h2>
 <h2 style="text-align: center"> TextBook</h2>
 <h3><a href="https://drive.google.com/file/d/1765a7bTY-nYGvFJMP41YahOQEy6gtGxN/view?usp=drive_link">Introduction to Autonomous Robots</a></h3>
 <p>By: Correll, Nikolaus and Hayes, Bradley, and Heckman, Christoffer, and Roncone, Alessandro</p>
